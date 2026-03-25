a28245775727e95281148dd8a0e2779cfb9209d4
Eletrica IMA <eletrica.ima@tevere.com.br>
2026-03-25 11:16:54 -0300

    Testes Produção config banco

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,7 +6,10 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 
 SECRET_KEY = os.getenv(
     "DJANGO_SECRET_KEY",
+    os.getenv(
+        "SECRET_KEY",
     "django-insecure-ew@5e7badi5^wq#)bzyrz!j%ea9rx*+9@2a8z(^7_+fd1@f2b7",
+    ),
 )
 
 
@@ -62,11 +65,22 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 ASGI_APPLICATION = "config.asgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
--- config/settings/production.py
+++ config/settings/production.py
@@ -4,15 +4,15 @@ import os
 
 DEBUG = False
 
-DATABASES={
-    "default":dj_database_url.config(
+DATABASES = {
+    "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True,
     ) 
 }
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "hhtps")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURA = True
+SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
