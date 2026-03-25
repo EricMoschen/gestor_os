410db277b0e8a140e7f95f651a21adad866b6eca
Eletrica IMA <eletrica.ima@tevere.com.br>
2026-03-25 10:55:59 -0300

    Config Testes Deploy 2

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,7 +81,8 @@ TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
